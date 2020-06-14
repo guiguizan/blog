3d7b439a778be7d1be93204c4c1be758398f87fd
guiguizan <guiguizanchettin@gmail.com>
2020-06-14 13:06:44 -0300

    login e outras paginas

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,10 @@ from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 
 
+def home(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/home.html', {'posts': posts})
+
 def post_list(request): 
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
     return render(request, 'blog/post_list.html', {'posts': posts})
